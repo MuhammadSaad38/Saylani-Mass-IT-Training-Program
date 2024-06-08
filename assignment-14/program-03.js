d41e052e90ee8fa96b0d4c8a1eec0868cf758ad9
MuhammadSaad38 <oraclea38@gmail.com>
2024-06-08 13:57:37 +0500

    add program 03

---
+++ assignment-14/program-03.js
@@ -0,0 +1,8 @@
+// 3. Write a js program to find sum of all array elements. 
+
+var arr = [1, 2, 3, 4, 5, 6]
+var sum = 0;
+for (var i=0; i < arr.length; i++){
+    sum = sum + arr[i]; 
+}
+console.log(sum);
